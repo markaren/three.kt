532d8cd0e2bb1d8ac76d59ce045e4aef7e9b107d
Lars Ivar Hatledal <laht@ntnu.no>
2019-07-24 10:50:06 +0200

    Add cylindrical

---
+++ src/main/kotlin/info/laht/threekt/math/Cylindrical.kt
@@ -0,0 +1,49 @@
+package info.laht.threekt.math
+
+import info.laht.threekt.core.Cloneable
+import kotlin.math.atan2
+import kotlin.math.sqrt
+
+class Cylindrical(
+        var radius: Float = 1f,
+        var theta: Float = 0f,
+        var y: Float = 0f
+): Cloneable {
+
+    fun set( radius: Float, theta: Float, y: Float ): Cylindrical {
+        this.radius = radius
+        this.theta = theta
+        this.y = y
+
+        return this
+    }
+
+    fun setFromVector3 ( v: Vector3 ): Cylindrical {
+
+        return this.setFromCartesianCoords( v.x, v.y, v.z );
+
+    }
+
+    fun setFromCartesianCoords ( x: Float, y: Float, z: Float ): Cylindrical {
+
+        this.radius = sqrt( x * x + z * z );
+        this.theta = atan2( x, z );
+        this.y = y;
+
+        return this;
+
+    }
+
+    override fun clone(): Cylindrical {
+        return Cylindrical(radius, theta, y)
+    }
+
+    fun copy( source: Cylindrical ): Cylindrical {
+        return set(source.radius, source.theta, source.y)
+    }
+
+    override fun toString(): String {
+        return "Cylindrical(radius=$radius, theta=$theta, y=$y)"
+    }
+
+}
