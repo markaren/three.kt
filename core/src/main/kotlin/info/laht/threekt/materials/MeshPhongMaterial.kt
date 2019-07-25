f66d669484188dce33b472dcdc479d5c2c26319b
Lars Ivar Hatledal <laht@hials.no>
2019-07-25 20:56:26 +0200

    Add MeshPhongMaterial

--- core/src/main/kotlin/info/laht/threekt/materials/MeshPhongMaterial.kt
+++ core/src/main/kotlin/info/laht/threekt/materials/MeshPhongMaterial.kt
@@ -1,3 +1,113 @@
 package info.laht.threekt.materials
 
-class MeshPhongMaterial: Material()
+import info.laht.threekt.MultiplyOperation
+import info.laht.threekt.TangentSpaceNormalMap
+import info.laht.threekt.math.Color
+import info.laht.threekt.math.Vector2
+import info.laht.threekt.textures.Texture
+
+class MeshPhongMaterial: Material(), MaterialWithWireframe, MaterialWithColor, MaterialWithEmissive, MaterialWithMorphTarget, MaterialWithMorphNormals, MaterialWithSkinning {
+
+    override var color =  Color( 0xffffff ) // diffuse
+    var specular =  Color( 0x111111 )
+    var shininess = 30f
+
+    public override var map: Texture? = null
+
+    public override var lightMap: Texture? = null
+    public override var lightMapIntensity = 1f
+
+    public override var aoMap: Texture? = null
+    public override var aoMapIntensity = 1f
+
+    override var emissive =  Color( 0x000000 )
+    override var emissiveIntensity = 1f
+    public override var emissiveMap: Texture? = null
+
+    public override var bumpMap: Texture? = null
+    public override var bumpScale = 1f
+
+    public override var normalMap: Texture? = null
+    public override var normalMapType = TangentSpaceNormalMap
+    var normalScale =  Vector2( 1, 1 )
+
+    public override var displacementMap: Texture? = null
+    public override var displacementScale = 1f
+    public override var displacementBias = 0f
+
+    public override var specularMap: Texture? = null
+
+    public override var alphaMap: Texture? = null
+
+    public override var envMap: Texture? = null
+    override var combine = MultiplyOperation
+    var reflectivity = 1f
+    var refractionRatio = 0.98f
+
+    override var wireframe = false
+    override var wireframeLinewidth = 1f
+    var wireframeLinecap = "round"
+    var wireframeLinejoin = "round"
+
+    override var skinning = false
+    override var morphTargets = false
+    override var morphNormals = false
+
+    override fun clone(): MeshPhongMaterial {
+        return MeshPhongMaterial().copy(this)
+    }
+
+    fun copy ( source: MeshPhongMaterial ): MeshPhongMaterial {
+
+        super.copy(source)
+
+        this.color.copy( source.color )
+        this.specular.copy( source.specular )
+        this.shininess = source.shininess
+
+        this.map = source.map
+
+        this.lightMap = source.lightMap
+        this.lightMapIntensity = source.lightMapIntensity
+
+        this.aoMap = source.aoMap
+        this.aoMapIntensity = source.aoMapIntensity
+
+        this.emissive.copy( source.emissive )
+        this.emissiveMap = source.emissiveMap
+        this.emissiveIntensity = source.emissiveIntensity
+
+        this.bumpMap = source.bumpMap
+        this.bumpScale = source.bumpScale
+
+        this.normalMap = source.normalMap
+        this.normalMapType = source.normalMapType
+        this.normalScale.copy( source.normalScale )
+
+        this.displacementMap = source.displacementMap
+        this.displacementScale = source.displacementScale
+        this.displacementBias = source.displacementBias
+
+        this.specularMap = source.specularMap
+
+        this.alphaMap = source.alphaMap
+
+        this.envMap = source.envMap
+        this.combine = source.combine
+        this.reflectivity = source.reflectivity
+        this.refractionRatio = source.refractionRatio
+
+        this.wireframe = source.wireframe
+        this.wireframeLinewidth = source.wireframeLinewidth
+        this.wireframeLinecap = source.wireframeLinecap
+        this.wireframeLinejoin = source.wireframeLinejoin
+
+        this.skinning = source.skinning
+        this.morphTargets = source.morphTargets
+        this.morphNormals = source.morphNormals
+
+        return this
+
+    }
+
+}
