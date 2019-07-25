bb8d39d65bcfbd309e92c2569569f3857abdbc2c
Lars Ivar Hatledal <laht@hials.no>
2019-07-25 10:30:14 +0200

    Add Color equals

--- core/src/main/kotlin/info/laht/threekt/math/Color.kt
+++ core/src/main/kotlin/info/laht/threekt/math/Color.kt
@@ -82,6 +82,26 @@ class Color(
         return Color(r, g, b)
     }
 
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Color
+
+        if (r != other.r) return false
+        if (g != other.g) return false
+        if (b != other.b) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = r.hashCode()
+        result = 31 * result + g.hashCode()
+        result = 31 * result + b.hashCode()
+        return result
+    }
+
     override fun toString(): String {
         return "Color(r=$r, g=$g, b=$b)"
     }
