3d2180138df856cefd67d6522668fbd6f1c4ffb6
Lars Ivar Hatledal <laht@hials.no>
2019-07-25 22:19:38 +0200

    update lambert material

--- core/src/main/kotlin/info/laht/threekt/materials/MeshLambertMaterial.kt
+++ core/src/main/kotlin/info/laht/threekt/materials/MeshLambertMaterial.kt
@@ -6,16 +6,63 @@ class MeshLambertMaterial : Material(), MaterialWithColor, MaterialWithSkinning,
 
     override val color = Color( 0xffffff )
 
-    override val emissive = Color( 0x000000 );
-    override var emissiveIntensity = 1f;
+    override val emissive = Color( 0x000000 )
+    override var emissiveIntensity = 1f
 
-    override var wireframe = false;
-    override var wireframeLinewidth = 1f;
-    var wireframeLinecap = "round";
-    var wireframeLinejoin = "round";
-    
+    var reflectivity = 1f
+    var refractionRatio = 0.98f
+
+    override var wireframe = false
+    override var wireframeLinewidth = 1f
+    var wireframeLinecap = "round"
+    var wireframeLinejoin = "round"
+
     override var skinning = false
     override var morphTargets = false
     override var morphNormals = false
+
+    override fun clone(): MeshLambertMaterial {
+        return MeshLambertMaterial().copy(this)
+    }
+
+    fun copy( source: MeshLambertMaterial ): MeshLambertMaterial {
+
+        super.copy(source)
+
+        this.color.copy( source.color )
+
+        this.map = source.map
+
+        this.lightMap = source.lightMap
+        this.lightMapIntensity = source.lightMapIntensity
+
+        this.aoMap = source.aoMap
+        this.aoMapIntensity = source.aoMapIntensity
+
+        this.emissive.copy( source.emissive )
+        this.emissiveMap = source.emissiveMap
+        this.emissiveIntensity = source.emissiveIntensity
+
+        this.specularMap = source.specularMap
+
+        this.alphaMap = source.alphaMap
+
+        this.envMap = source.envMap
+        this.combine = source.combine
+        this.reflectivity = source.reflectivity
+        this.refractionRatio = source.refractionRatio
+
+        this.wireframe = source.wireframe
+        this.wireframeLinewidth = source.wireframeLinewidth
+        this.wireframeLinecap = source.wireframeLinecap
+        this.wireframeLinejoin = source.wireframeLinejoin
+
+        this.skinning = source.skinning
+        this.morphTargets = source.morphTargets
+        this.morphNormals = source.morphNormals
+
+        return this
+
+    }
 
 }
