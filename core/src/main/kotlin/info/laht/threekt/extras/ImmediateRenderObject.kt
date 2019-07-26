9f3c704dda3196d5a7f2eb9a3adf20c25a81d8c4
Lars Ivar Hatledal <laht@hials.no>
2019-07-26 10:43:35 +0200

    Add ImmediateRenderObject

---
+++ core/src/main/kotlin/info/laht/threekt/extras/ImmediateRenderObject.kt
@@ -0,0 +1,13 @@
+package info.laht.threekt.extras
+
+import info.laht.threekt.core.MaterialObject
+import info.laht.threekt.core.Object3D
+import info.laht.threekt.materials.Material
+
+class ImmediateRenderObject(
+    override val material: Material
+): Object3D(), MaterialObject {
+
+    var render: () -> Unit = {}
+
+}
