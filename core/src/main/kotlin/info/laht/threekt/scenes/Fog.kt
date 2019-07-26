c4efd17f83da8045d9e0c605c8416e67096a4988
Lars Ivar Hatledal <laht@hials.no>
2019-07-26 22:22:01 +0200

    Update Fog

--- core/src/main/kotlin/info/laht/threekt/scenes/Fog.kt
+++ core/src/main/kotlin/info/laht/threekt/scenes/Fog.kt
@@ -15,9 +15,14 @@ sealed class _Fog(
 
 class Fog(
     color: Color,
-    val near: Float = 1f,
-    val far: Float = 1000f
+    near: Number? = null,
+    far: Number? = null
 ): _Fog(color) {
+
+    val near = near?.toFloat() ?: 1f
+    val far = far?.toFloat() ?: 1000f
+
+    constructor(color: Int, near: Number? = null, far: Number? = null): this(Color(color), near, far)
 
     override fun clone(): Fog {
         return Fog(color, near, far)
@@ -27,10 +32,12 @@ class Fog(
 
 class FogExp2(
     color: Color,
-    density: Float? = null
+    density: Number? = null
 ): _Fog(color) {
 
-    var density = density ?: 0.00025f
+    var density = density?.toFloat() ?: 0.00025f
+
+    constructor(color: Int, density: Float?): this(Color(color), density)
 
     override fun clone(): FogExp2 {
         return FogExp2(color.clone(), density)
