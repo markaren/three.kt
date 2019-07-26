e2ff105da4db8a09caf9ac0882bad053fe981670
Lars Ivar Hatledal <laht@hials.no>
2019-07-26 22:23:05 +0200

    Add PointsExample

---
+++ examples/src/main/kotlin/info/laht/threekt/examples/objects/PointsExample.kt
@@ -0,0 +1,87 @@
+package info.laht.threekt.examples.objects
+
+import info.laht.threekt.Canvas
+import info.laht.threekt.CanvasOptions
+import info.laht.threekt.VertexColors
+import info.laht.threekt.cameras.PerspectiveCamera
+import info.laht.threekt.core.BufferGeometry
+import info.laht.threekt.core.Clock
+import info.laht.threekt.core.FloatBufferAttribute
+import info.laht.threekt.materials.PointsMaterial
+import info.laht.threekt.objects.Points
+import info.laht.threekt.renderers.GLRenderer
+import info.laht.threekt.scenes.ColorBackground
+import info.laht.threekt.scenes.Fog
+import info.laht.threekt.scenes.Scene
+import kotlin.random.Random
+
+object PointsExample {
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+
+        Canvas(CanvasOptions().apply {
+            antialiasing = 4
+        }).use { canvas ->
+
+            val scene = Scene().apply {
+                background = ColorBackground(0x050505)
+                fog = Fog(0x050505, 2000, 3500)
+            }
+            val renderer = GLRenderer(canvas)
+
+            val camera = PerspectiveCamera(27, canvas.aspect, 5, 3500).apply {
+                position.z = 2750f
+            }
+
+            val numParticles = 500000
+            val positions = FloatArray(numParticles * 3)
+            val colors = FloatArray(numParticles * 3)
+
+            val n = 1000
+            val n2 = n / 2
+
+            for (i in 0 until numParticles step 3) {
+
+                positions[i] = Random.nextFloat() * n - n2
+                positions[i + 1] = Random.nextFloat() * n - n2
+                positions[i + 2] = Random.nextFloat() * n - n2
+
+                colors[i] = (positions[i] / n) + 0.5f
+                colors[i + 1] = (positions[i + 1] / n) + 0.5f
+                colors[i + 2] = (positions[i + 2] / n) + 0.5f
+
+            }
+
+
+            val geometry = BufferGeometry()
+            geometry.addAttribute("position", FloatBufferAttribute(positions, 3))
+            geometry.addAttribute("color", FloatBufferAttribute(colors, 3))
+
+            geometry.computeBoundingSphere()
+
+            val material = PointsMaterial().apply {
+                size = 15f
+                vertexColors = VertexColors
+            }
+
+            val points = Points(geometry, material)
+            scene.add(points)
+
+            val clock = Clock()
+            while (!canvas.shouldClose()) {
+
+                val time = clock.getElapsedTime()
+
+                points.rotation.x = time * 0.25f
+                points.rotation.y = time * 0.5f
+
+                renderer.render(scene, camera)
+
+            }
+
+        }
+
+    }
+
+}
