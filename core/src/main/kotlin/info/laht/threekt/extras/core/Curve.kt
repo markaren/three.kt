adcd7790fd619583347991ed3078a8e454498685
Lars Ivar Hatledal <laht@ntnu.no>
2019-07-29 14:47:29 +0200

    Add more Curve functions

--- core/src/main/kotlin/info/laht/threekt/extras/core/Curve.kt
+++ core/src/main/kotlin/info/laht/threekt/extras/core/Curve.kt
@@ -1,6 +1,10 @@
 package info.laht.threekt.extras.core
 
+import info.laht.threekt.math.Matrix4
 import info.laht.threekt.math.Vector3
+import info.laht.threekt.math.clamp
+import kotlin.math.abs
+import kotlin.math.acos
 
 abstract class Curve3 {
 
@@ -19,7 +23,7 @@ abstract class Curve3 {
         return getPoint(t, optionalTarget)
     }
 
-    fun getPoints ( divisions: Int = 5 ): List<Vector3> {
+    fun getPoints(divisions: Int = 5): List<Vector3> {
 
         return (0..divisions).map {
             getPoint(it.toFloat())
@@ -27,7 +31,7 @@ abstract class Curve3 {
 
     }
 
-    fun getSpacedPoints ( divisions: Int = 5 ): List<Vector3> {
+    fun getSpacedPoints(divisions: Int = 5): List<Vector3> {
 
         return (0..divisions).map {
             getPointAt(it.toFloat())
@@ -43,34 +47,34 @@ abstract class Curve3 {
     fun getLengths(divisions: Int = arcLengthDivisions): FloatArray {
 
         if (this.cacheArcLengths != null &&
-            (this.cacheArcLengths?.size == divisions + 1) &&
-            !this.needsUpdate
+                (this.cacheArcLengths?.size == divisions + 1) &&
+                !this.needsUpdate
         ) {
 
-            return this.cacheArcLengths!!;
+            return this.cacheArcLengths!!
 
         }
 
-        this.needsUpdate = false;
+        this.needsUpdate = false
 
-        val cache = FloatArray(divisions);
-        var last = this.getPoint(0f);
-        var sum = 0f;
+        val cache = FloatArray(divisions)
+        var last = this.getPoint(0f)
+        var sum = 0f
 
-        cache[0] = 0f;
+        cache[0] = 0f
 
         for (p in 1..divisions) {
 
-            val current = this.getPoint((p / divisions).toFloat());
-            sum += current.distanceTo(last);
-            cache[p] = sum;
-            last = current;
+            val current = this.getPoint((p / divisions).toFloat())
+            sum += current.distanceTo(last)
+            cache[p] = sum
+            last = current
 
         }
 
-        this.cacheArcLengths = cache;
+        this.cacheArcLengths = cache
 
-        return cache; // { sums: cache, sum: sum }; Sum is in the last element.
+        return cache // { sums: cache, sum: sum }; Sum is in the last element.
 
     }
 
@@ -80,11 +84,11 @@ abstract class Curve3 {
     }
 
     fun getUtoTmapping(u: Float, distance: Float? = null): Float {
-        val arcLengths = this.getLengths();
+        val arcLengths = this.getLengths()
 
         val il = arcLengths.size
 
-        val targetArcLength = if (distance != null) distance else u * arcLengths[il - 1];
+        val targetArcLength = if (distance != null) distance else u * arcLengths[il - 1]
 
         // binary search for the index with largest value smaller than target u distance
 
@@ -96,20 +100,20 @@ abstract class Curve3 {
 
             i = low + (high - low) / 2
 
-            val comparison = arcLengths[i] - targetArcLength;
+            val comparison = arcLengths[i] - targetArcLength
 
             if (comparison < 0) {
 
-                low = i + 1;
+                low = i + 1
 
             } else if (comparison > 0) {
 
-                high = i - 1;
+                high = i - 1
 
             } else {
 
-                high = i;
-                break;
+                high = i
+                break
 
                 // DONE
 
@@ -117,30 +121,181 @@ abstract class Curve3 {
 
         }
 
-        i = high;
+        i = high
 
         if (arcLengths[i] == targetArcLength) {
 
-            return i.toFloat() / (il - 1);
+            return i.toFloat() / (il - 1)
 
         }
 
         // we could get finer grain at lengths, or use simple interpolation between two points
 
-        val lengthBefore = arcLengths[i];
-        val lengthAfter = arcLengths[i + 1];
+        val lengthBefore = arcLengths[i]
+        val lengthAfter = arcLengths[i + 1]
 
-        val segmentLength = lengthAfter - lengthBefore;
+        val segmentLength = lengthAfter - lengthBefore
 
         // determine where we are between the 'before' and 'after' points
 
-        val segmentFraction = (targetArcLength - lengthBefore) / segmentLength;
+        val segmentFraction = (targetArcLength - lengthBefore) / segmentLength
 
         // add that fractional amount to t
 
-        val t = (i + segmentFraction) / (il - 1);
+        val t = (i + segmentFraction) / (il - 1)
 
-        return t;
+        return t
+    }
+
+    /**
+     *  Returns a unit vector tangent at t
+     *  In case any sub curve does not implement its tangent derivation,
+     *  2 points a small delta apart will be used to find its gradient
+     *  which seems to give a reasonable approximation
+     */
+    fun getTangent(t: Float): Vector3 {
+
+        val delta = 0.0001f
+        var t1 = t - delta
+        var t2 = t + delta
+
+        // Capping in case of danger
+
+        if (t1 < 0) t1 = 0f
+        if (t2 > 1) t2 = 1f
+
+        val pt1 = this.getPoint(t1)
+        val pt2 = this.getPoint(t2)
+
+        val vec = pt2.clone().sub(pt1)
+        return vec.normalize()
+
+    }
+
+    fun getTangentAt(u: Float): Vector3 {
+
+        val t = this.getUtoTmapping(u)
+        return this.getTangent(t)
+
+    }
+
+    fun computeFrenetFrames(segments: Int, closed: Boolean): FrenetFrames {
+
+        // see http://www.cs.indiana.edu/pub/techreports/TR425.pdf
+
+        val normal = Vector3()
+
+        val tangents = mutableListOf<Vector3>()
+        val normals = mutableListOf<Vector3>()
+        val binormals = mutableListOf<Vector3>()
+
+        val vec = Vector3()
+        val mat = Matrix4()
+
+        // compute the tangent vectors for each segment on the curve
+
+        for (i in 0..segments) {
+
+            val u = i.toFloat() / segments
+
+            tangents[i] = this.getTangentAt(u)
+            tangents[i].normalize()
+
+        }
+
+        // select an initial normal vector perpendicular to the first tangent vector,
+        // and in the direction of the minimum tangent xyz component
+
+        normals.add(Vector3())
+        binormals.add(Vector3())
+        var min = Float.MAX_VALUE
+        val tx = abs(tangents[0].x)
+        val ty = abs(tangents[0].y)
+        val tz = abs(tangents[0].z)
+
+        if (tx <= min) {
+
+            min = tx
+            normal.set(1, 0, 0)
+
+        }
+
+        if (ty <= min) {
+
+            min = ty
+            normal.set(0, 1, 0)
+
+        }
+
+        if (tz <= min) {
+
+            normal.set(0, 0, 1)
+
+        }
+
+        vec.crossVectors(tangents[0], normal).normalize()
+
+        normals[0].crossVectors(tangents[0], vec)
+        binormals[0].crossVectors(tangents[0], normals[0])
+
+
+        // compute the slowly-varying normal and binormal vectors for each segment on the curve
+
+        var theta = 0f
+
+        for (i in 1..segments) {
+
+            normals[i] = normals[i - 1].clone()
+
+            binormals[i] = binormals[i - 1].clone()
+
+            vec.crossVectors(tangents[i - 1], tangents[i])
+
+            if (vec.length() > 1E-14 /*Number.EPSILON*/) {
+
+                vec.normalize()
+
+                theta = acos(clamp(tangents[i - 1].dot(tangents[i]), -1, 1)) // clamp for floating pt errors
+
+                normals[i].applyMatrix4(mat.makeRotationAxis(vec, theta))
+
+            }
+
+            binormals[i].crossVectors(tangents[i], normals[i])
+
+        }
+
+        // if the curve is closed, postprocess the vectors so the first and last normal vectors are the same
+
+        if (closed) {
+
+            var theta = acos(clamp(normals[0].dot(normals[segments]), -1, 1))
+            theta /= segments
+
+            if (tangents[0].dot(vec.crossVectors(normals[0], normals[segments])) > 0) {
+
+                theta = -theta
+
+            }
+
+            for (i in 1..segments) {
+
+                // twist a little...
+                normals[i].applyMatrix4(mat.makeRotationAxis(tangents[i], theta * i))
+                binormals[i].crossVectors(tangents[i], normals[i])
+
+            }
+
+        }
+
+        return FrenetFrames(tangents, normals, binormals)
+
     }
 
 }
+
+class FrenetFrames(
+        val tangents: List<Vector3>,
+        val normals: List<Vector3>,
+        val binormals: List<Vector3>
+)
