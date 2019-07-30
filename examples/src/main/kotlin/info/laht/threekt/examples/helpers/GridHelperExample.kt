6c29be21dfd256553f1e5dccb1b6f828a79e4b50
Lars Ivar Hatledal <laht@hials.no>
2019-07-30 20:44:57 +0200

    Update example

--- examples/src/main/kotlin/info/laht/threekt/examples/helpers/GridHelperExample.kt
+++ examples/src/main/kotlin/info/laht/threekt/examples/helpers/GridHelperExample.kt
@@ -2,15 +2,11 @@ package info.laht.threekt.examples.helpers
 
 import info.laht.threekt.Canvas
 import info.laht.threekt.CanvasOptions
-import info.laht.threekt.DoubleSide
 import info.laht.threekt.cameras.PerspectiveCamera
 import info.laht.threekt.controls.OrbitControls
-import info.laht.threekt.geometries.PlaneGeometry
+import info.laht.threekt.helpers.AxesHelper
 import info.laht.threekt.helpers.GridHelper
-import info.laht.threekt.materials.MeshBasicMaterial
 import info.laht.threekt.math.Color
-import info.laht.threekt.math.DEG2RAD
-import info.laht.threekt.objects.Mesh
 import info.laht.threekt.renderers.GLRenderer
 import info.laht.threekt.scenes.Scene
 
@@ -29,21 +25,19 @@ object GridHelperExample {
                 it.translateZ(10f).translateY(5f)
             }
             val renderer = GLRenderer(canvas)
-            val controls = OrbitControls(camera, canvas)
+            OrbitControls(camera, canvas)
 
-            Mesh(PlaneGeometry(10f, 10f), MeshBasicMaterial().apply {
-                color.set(Color.gray)
-                side = DoubleSide
-            }).also {
-                it.rotation.x = DEG2RAD * -90
-                it.translateZ(-1f)
-                scene.add(it)
-            }
-
-            GridHelper().also {
-                it.translateY(-0.99f)
+            GridHelper(
+                color1 = Color(0x0000ff),
+                color2 = Color(0xffffff)
+            ).also {
                 scene.add(it)
                 camera.lookAt(it.position)
+            }
+
+            AxesHelper(2.5).also {
+                it.position.set(-5f, 0.1f, -5f)
+                scene.add(it)
             }
 
             while (!canvas.shouldClose()) {
