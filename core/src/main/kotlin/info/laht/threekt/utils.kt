6f13e493191a7676987adf3965e76e525c318be6
Lars Ivar Hatledal <laht@ntnu.no>
2019-07-31 15:32:43 +0200

    Update utils

--- core/src/main/kotlin/info/laht/threekt/utils.kt
+++ core/src/main/kotlin/info/laht/threekt/utils.kt
@@ -10,19 +10,28 @@ internal inline fun <reified T> MutableList<T?>.length(length: Int) {
             add(null)
         }
     } else if (length > size) {
-        removeAt(length - 1)
+        while(length > size) {
+            removeAt(length - 1)
+        }
     }
 
 }
 
-internal inline fun <reified T> MutableList<T?>.safeSet(index: Int, value: T) {
+internal inline fun <reified T> MutableList<T>.shrinkToFit(length: Int) {
+
+    if (length > size) {
+        while(length > size) {
+            removeAt(length - 1)
+        }
+    }
+
+}
+
+internal inline fun <reified T> MutableList<T>.safeSet(index: Int, value: T) {
 
     when {
         index < size -> set(index, value)
         index == size -> add(value)
-        else -> while (index > size) {
-            if (index == size) add(value) else add(null)
-        }
     }
 
 }
@@ -36,4 +45,26 @@ internal inline fun <reified T> MutableList<T>.add(v1: T, v2: T, v3: T) {
     add(v1)
     add(v2)
     add(v3)
+}
+
+internal inline fun <reified T> MutableList<T>.safeSet(v1: T) {
+    if (size > 0) set(0, v1) else add(v1)
+}
+
+internal inline fun <reified T> MutableList<T>.safeSet(v1: T, v2: T) {
+    if (size > 0) set(0, v1) else add(v1)
+    if (size > 1) set(1, v2) else add(v2)
+}
+
+internal inline fun <reified T> MutableList<T>.safeSet(v1: T, v2: T, v3: T) {
+    if (size > 0) set(0, v1) else add(v1)
+    if (size > 1) set(1, v2) else add(v2)
+    if (size > 2) set(2, v3) else add(v3)
+}
+
+internal inline fun <reified T> MutableList<T>.safeSet(v1: T, v2: T, v3: T, v4: T) {
+    if (size > 0) set(0, v1) else add(v1)
+    if (size > 1) set(1, v2) else add(v2)
+    if (size > 2) set(2, v3) else add(v3)
+    if (size > 3) set(3, v4) else add(v4)
 }
