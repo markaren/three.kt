c30e4f49ece5cb999897290ed2e93c09e767eb62
Lars Ivar Hatledal <laht@hials.no>
2019-07-31 21:23:34 +0200

    Cleanup

--- core/src/jvmMain/kotlin/info/laht/threekt/math/Math.kt
+++ core/src/jvmMain/kotlin/info/laht/threekt/math/Math.kt
@@ -21,15 +21,7 @@ fun clamp(value: Float, min: Number, max: Number): Float {
     return max(min.toFloat(), min(max.toFloat(), value))
 }
 
-fun clamp(value: Double, min: Number, max: Number): Double {
-    return max(min.toDouble(), min(max.toDouble(), value))
-}
-
 fun mapLinear(x: Float, a1: Float, a2: Float, b1: Float, b2: Float): Float {
-    return b1 + ( x - a1 ) * ( b2 - b1 ) / ( a2 - a1 )
-}
-
-fun mapLinear(x: Double, a1: Double, a2: Double, b1: Double, b2: Double): Double {
     return b1 + ( x - a1 ) * ( b2 - b1 ) / ( a2 - a1 )
 }
 
@@ -37,16 +29,12 @@ fun lerp(x: Float, y: Float, t: Float): Float {
     return ( 1 - t ) * x + t * y
 }
 
-fun lerp(x: Double, y: Double, t: Double): Double {
-    return ( 1 - t ) * x + t * y
-}
-
-fun smoothStep (x: Double, min: Double, max: Double): Double {
+fun smoothStep (x: Float, min: Float, max: Float): Float {
     if ( x <= min ) {
-        return 0.0
+        return 0f
     }
     if ( x >= max ) {
-        return 1.0
+        return 1f
     }
 
     @Suppress("NAME_SHADOWING")
@@ -55,27 +43,14 @@ fun smoothStep (x: Double, min: Double, max: Double): Double {
     return x * x * ( 3 - 2 * x )
 }
 
-fun smootherStep (x: Double, min: Double, max: Double): Double {
-    if ( x <= min ) {
-        return 0.0
-    }
-    if ( x >= max ) {
-        return 1.0
-    }
-
-    val x = ( x - min ) / ( max - min )
-
-    return x * x * x * ( x * ( x * 6 - 15 ) + 10 )
-}
-
 fun isPowerOfTwo(value: Int): Boolean {
     return ( value and ( value - 1 ) ) == 0 && value != 0
 }
 
-fun ceilPowerOfTwo(value: Double): Double {
-    return 2.0.pow(ceil(ln(value) / LN2))
+fun ceilPowerOfTwo(value: Float): Float {
+    return 2f.pow(ceil(ln(value) / LN2))
 }
 
-fun floorPowerOfTwo(value: Double): Double {
-    return 2.0.pow(floor(ln(value) / LN2))
+fun floorPowerOfTwo(value: Float): Float {
+    return 2f.pow(floor(ln(value) / LN2))
 }
