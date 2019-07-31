79b4fbea76fd47b896b268ee124dd84598dc7b34
Lars Ivar Hatledal <laht@hials.no>
2019-07-31 21:43:35 +0200

    Implement generateUUID in pure kotlin

--- core/src/jvmMain/kotlin/info/laht/threekt/math/Math.kt
+++ core/src/jvmMain/kotlin/info/laht/threekt/math/Math.kt
@@ -1,16 +1,33 @@
 package info.laht.threekt.math
 
-import java.util.*
 import kotlin.math.*
+import kotlin.random.Random
 
 val LN2 = ln(2.0).toFloat()
 const val DEG2RAD = (PI / 180.0).toFloat()
-const val RAD2DEG =  (180.0 / PI).toFloat()
+const val RAD2DEG = (180.0 / PI).toFloat()
 
 const val TWO_PI = (PI * 2).toFloat()
 
+private val lut by lazy {
+    Array(256) { i ->
+        if (i < 16) "0" else i.toString(16)
+    }
+}
+
 fun generateUUID(): String {
-    return UUID.randomUUID().toString()
+
+    val d0 = (Random.nextDouble() * 0xffffffff).toInt() or 0
+    val d1 = (Random.nextDouble() * 0xffffffff).toInt() or 0
+    val d2 = (Random.nextDouble() * 0xffffffff).toInt() or 0
+    val d3 = (Random.nextDouble() * 0xffffffff).toInt() or 0
+
+    val uuid = lut[d0 and 0xff] + lut[d0 shr 8 and 0xff] + lut[d0 shr 16 and 0xff] + lut[d0 shr 24 and 0xff] + '-' +
+            lut[d1 and 0xff] + lut[d1 shr 8 and 0xff] + '-' + lut[d1 shr 16 and 0x0f or 0x40] + lut[d1 shr 24 and 0xff] + '-' +
+            lut[d2 and 0x3f or 0x80] + lut[d2 shr 8 and 0xff] + '-' + lut[d2 shr 16 and 0xff] + lut[d2 shr 24 and 0xff] +
+            lut[d3 and 0xff] + lut[d3 shr 8 and 0xff] + lut[d3 shr 16 and 0xff] + lut[d3 shr 24 and 0xff]
+
+    return uuid.toUpperCase()
 }
 
 fun clamp(value: Int, min: Int, max: Int): Int {
@@ -22,29 +39,29 @@ fun clamp(value: Float, min: Number, max: Number): Float {
 }
 
 fun mapLinear(x: Float, a1: Float, a2: Float, b1: Float, b2: Float): Float {
-    return b1 + ( x - a1 ) * ( b2 - b1 ) / ( a2 - a1 )
+    return b1 + (x - a1) * (b2 - b1) / (a2 - a1)
 }
 
 fun lerp(x: Float, y: Float, t: Float): Float {
-    return ( 1 - t ) * x + t * y
+    return (1 - t) * x + t * y
 }
 
-fun smoothStep (x: Float, min: Float, max: Float): Float {
-    if ( x <= min ) {
+fun smoothStep(x: Float, min: Float, max: Float): Float {
+    if (x <= min) {
         return 0f
     }
-    if ( x >= max ) {
+    if (x >= max) {
         return 1f
     }
 
     @Suppress("NAME_SHADOWING")
-    val x = ( x - min ) / ( max - min )
+    val x = (x - min) / (max - min)
 
-    return x * x * ( 3 - 2 * x )
+    return x * x * (3 - 2 * x)
 }
 
 fun isPowerOfTwo(value: Int): Boolean {
-    return ( value and ( value - 1 ) ) == 0 && value != 0
+    return (value and (value - 1)) == 0 && value != 0
 }
 
 fun ceilPowerOfTwo(value: Float): Float {
