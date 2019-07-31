098a447ea9980eff52ee6964e1c8f6ab297e23d6
Lars Ivar Hatledal <laht@ntnu.no>
2019-07-31 11:26:26 +0200

    Remove reflection helper

--- core/src/main/kotlin/info/laht/threekt/utils.kt
+++ core/src/main/kotlin/info/laht/threekt/utils.kt
@@ -1,26 +1,5 @@
 package info.laht.threekt
 
-import java.lang.reflect.Field
-
-/**
- * Returns the first [Field] in the hierarchy for the specified name
- */
-internal fun getFieldInHiarchy(clazz: Class<*>?, name: String): Field? {
-
-    @Suppress("NAME_SHADOWING")
-    var clazz: Class<*>? = clazz
-    var field: Field? = null
-    while (clazz != null && field == null) {
-        try {
-            field = clazz.getDeclaredField(name)
-        } catch (e: Exception) {
-        }
-
-        clazz = clazz.superclass
-    }
-    return field
-}
-
 internal val List<*>.length: Int
     get() = this.size
 
