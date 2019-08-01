fc7758ccf9799bdc246e231cd9ebf5187f191f0f
Lars Ivar Hatledal <laht@ntnu.no>
2019-08-01 08:39:44 +0200

    Optimize

--- core/src/commonMain/kotlin/info/laht/threekt/utils.kt
+++ core/src/commonMain/kotlin/info/laht/threekt/utils.kt
@@ -10,7 +10,7 @@ internal inline fun <reified T> MutableList<T?>.length(length: Int) {
             add(null)
         }
     } else if (length > size) {
-        while(length > size) {
+        while (length > size) {
             removeAt(length - 1)
         }
     }
@@ -20,7 +20,7 @@ internal inline fun <reified T> MutableList<T?>.length(length: Int) {
 internal inline fun <reified T> MutableList<T>.shrinkToFit(length: Int) {
 
     if (length > size) {
-        while(length > size) {
+        while (length > size) {
             removeAt(length - 1)
         }
     }
@@ -48,23 +48,72 @@ internal inline fun <reified T> MutableList<T>.add(v1: T, v2: T, v3: T) {
 }
 
 internal inline fun <reified T> MutableList<T>.safeSet(v1: T) {
-    if (size > 0) set(0, v1) else add(v1)
+    if (size == 0) {
+        add(v1)
+    } else {
+        set(0, v1)
+    }
 }
 
 internal inline fun <reified T> MutableList<T>.safeSet(v1: T, v2: T) {
-    if (size > 0) set(0, v1) else add(v1)
-    if (size > 1) set(1, v2) else add(v2)
+    if (size == 0) {
+        add(v1)
+        add(v2)
+    } else if (size == 1) {
+        set(0, v1)
+        add(v2)
+    } else {
+        set(0, v1)
+        set(1, v2)
+
+    }
 }
 
 internal inline fun <reified T> MutableList<T>.safeSet(v1: T, v2: T, v3: T) {
-    if (size > 0) set(0, v1) else add(v1)
-    if (size > 1) set(1, v2) else add(v2)
-    if (size > 2) set(2, v3) else add(v3)
+    if (size == 0) {
+        add(v1)
+        add(v2)
+        add(v3)
+    } else if (size == 1) {
+        set(0, v1)
+        add(v2)
+        add(v3)
+    } else if (size == 2) {
+        set(0, v1)
+        set(1, v2)
+        add(v3)
+    } else {
+        set(0, v1)
+        set(1, v2)
+        set(2, v3)
+    }
 }
 
 internal inline fun <reified T> MutableList<T>.safeSet(v1: T, v2: T, v3: T, v4: T) {
-    if (size > 0) set(0, v1) else add(v1)
-    if (size > 1) set(1, v2) else add(v2)
-    if (size > 2) set(2, v3) else add(v3)
-    if (size > 3) set(3, v4) else add(v4)
+    if (size == 0) {
+        add(v1)
+        add(v2)
+        add(v3)
+        add(v4)
+    } else if (size == 1) {
+        set(0, v1)
+        add(v2)
+        add(v3)
+        add(v4)
+    } else if (size == 2) {
+        set(0, v1)
+        set(1, v2)
+        add(v3)
+        add(v4)
+    } else if (size == 3) {
+        set(0, v1)
+        set(1, v2)
+        set(2, v3)
+        add(v4)
+    } else {
+        set(0, v1)
+        set(1, v2)
+        set(2, v3)
+        set(3, v4)
+    }
 }
