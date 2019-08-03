73d5185bc710a43719907c89e98fc9dc6a63e84e
Lars Ivar Hatledal <laht@hials.no>
2019-08-03 11:29:27 +0200

    Use Kotlin regex and simplify

--- core/src/jvmMain/kotlin/info/laht/threekt/loaders/OBJLoader.kt
+++ core/src/jvmMain/kotlin/info/laht/threekt/loaders/OBJLoader.kt
@@ -4,25 +4,24 @@ import info.laht.threekt.core.BufferGeometry
 import info.laht.threekt.core.FloatBufferAttribute
 import info.laht.threekt.core.IntBufferAttribute
 import java.io.File
-import java.util.regex.Pattern
 
-private val VERTEX_PATTERN = "v( +[\\d|\\.|\\+|\\-|e|E]+)( +[\\d|\\.|\\+|\\-|e|E]+)( +[\\d|\\.|\\+|\\-|e|E]+)"
-private val NORMAL_PATTERN = "vn( +[\\d|\\.|\\+|\\-|e|E]+)( +[\\d|\\.|\\+|\\-|e|E]+)( +[\\d|\\.|\\+|\\-|e|E]+)"
+private val VERTEX_PATTERN = "v( +[\\d|.|+|\\-|e|E]+)( +[\\d|.|+|\\-|e|E]+)( +[\\d|.|+|\\-|e|E]+)"
+private val NORMAL_PATTERN = "vn( +[\\d|.|+|\\-|e|E]+)( +[\\d|.|+|\\-|e|E]+)( +[\\d|.|+|\\-|e|E]+)"
 
 // f vertex vertex vertex ...
 private val FACE_PATTERN1 = "f( +-?\\d+)( +-?\\d+)( +-?\\d+)( +-?\\d+)?"
 
 // f vertex/uv vertex/uv vertex/uv ...
 private val FACE_PATTERN2 =
-    "f( +(-?\\d+)\\/(-?\\d+))( +(-?\\d+)\\/(-?\\d+))( +(-?\\d+)\\/(-?\\d+))( +(-?\\d+)\\/(-?\\d+))?"
+    "f( +(-?\\d+)/(-?\\d+))( +(-?\\d+)/(-?\\d+))( +(-?\\d+)/(-?\\d+))( +(-?\\d+)/(-?\\d+))?"
 
 // f vertex/uv/normal vertex/uv/normal vertex/uv/normal ...
 private val FACE_PATTERN3 =
-    "f( +(-?\\d+)\\/(-?\\d+)\\/(-?\\d+))( +(-?\\d+)\\/(-?\\d+)\\/(-?\\d+))( +(-?\\d+)\\/(-?\\d+)\\/(-?\\d+))( +(-?\\d+)\\/(-?\\d+)\\/(-?\\d+))?"
+    "f( +(-?\\d+)/(-?\\d+)/(-?\\d+))( +(-?\\d+)/(-?\\d+)/(-?\\d+))( +(-?\\d+)/(-?\\d+)/(-?\\d+))( +(-?\\d+)/(-?\\d+)/(-?\\d+))?"
 
 // f vertex//normal vertex//normal vertex//normal ...
 private val FACE_PATTERN4 =
-    "f( +(-?\\d+)\\/\\/(-?\\d+))( +(-?\\d+)\\/\\/(-?\\d+))( +(-?\\d+)\\/\\/(-?\\d+))( +(-?\\d+)\\/\\/(-?\\d+))?"
+    "f( +(-?\\d+)//(-?\\d+))( +(-?\\d+)//(-?\\d+))( +(-?\\d+)//(-?\\d+))( +(-?\\d+)//(-?\\d+))?"
 
 class OBJLoader {
 
@@ -38,80 +37,47 @@ class OBJLoader {
         val vertices = ArrayList<Float>()
         val normals = ArrayList<Float>()
 
-        run {
-            val compile = Pattern.compile(VERTEX_PATTERN)
-            val matcher = compile.matcher(text)
-
-            while (matcher.find()) {
-                val result = matcher.group().replace("  ", " ").split(" ")
-                vertices.add(result[1].toFloat())
-                vertices.add(result[2].toFloat())
-                vertices.add(result[3].toFloat())
-            }
+        VERTEX_PATTERN.toRegex().findAll(text).forEach {
+            val result = it.groups[0]!!.value.replace("  ", " ").split(" ")
+            vertices.add(result[1].toFloat())
+            vertices.add(result[2].toFloat())
+            vertices.add(result[3].toFloat())
         }
 
-        run {
-            val compile = Pattern.compile(NORMAL_PATTERN)
-            val matcher = compile.matcher(text)
-
-            while (matcher.find()) {
-                val result = matcher.group().replace("  ", " ").split(" ")
-                normals.add(result[1].toFloat())
-                normals.add(result[2].toFloat())
-                normals.add(result[3].toFloat())
-            }
+        NORMAL_PATTERN.toRegex().findAll(text).forEach {
+            val result = it.groups[0]!!.value.replace("  ", " ").split(" ")
+            normals.add(result[1].toFloat())
+            normals.add(result[2].toFloat())
+            normals.add(result[3].toFloat())
         }
 
-        run {
-            val compile = Pattern.compile(FACE_PATTERN1)
-            val matcher = compile.matcher(text)
-
-            while (matcher.find()) {
-                val result = matcher.group().split(" ")
-                indices.add(result[1].toInt() - 1)
-                indices.add(result[2].toInt() - 1)
-                indices.add(result[3].toInt() - 1)
-            }
-
+        FACE_PATTERN1.toRegex().findAll(text).forEach {
+            val result = it.groups[0]!!.value.split(" ")
+            indices.add(result[1].toInt() - 1)
+            indices.add(result[2].toInt() - 1)
+            indices.add(result[3].toInt() - 1)
         }
 
-        run {
-            val compile = Pattern.compile(FACE_PATTERN2)
-            val matcher = compile.matcher(text)
+        FACE_PATTERN2.toRegex().findAll(text).forEach {
+            val result = it.groups[0]!!.value.split(" ")
 
-            while (matcher.find()) {
-                val result = matcher.group().split(" ")
-                indices.add(result[1].split("/".toRegex()).dropLastWhile({ it.isEmpty() })[0].toInt() - 1)
-                indices.add(result[2].split("/".toRegex()).dropLastWhile({ it.isEmpty() })[0].toInt() - 1)
-                indices.add(result[3].split("/".toRegex()).dropLastWhile({ it.isEmpty() })[0].toInt() - 1)
-            }
-
+            indices.add(result[1].split("/")[0].toInt() - 1)
+            indices.add(result[2].split("/")[0].toInt() - 1)
+            indices.add(result[3].split("/")[0].toInt() - 1)
         }
 
-        run {
-            val compile = Pattern.compile(FACE_PATTERN3)
-            val matcher = compile.matcher(text)
-
-            while (matcher.find()) {
-                val result = matcher.group().split(" ")
-                indices.add(result[1].split("/".toRegex()).dropLastWhile({ it.isEmpty() })[0].toInt() - 1)
-                indices.add(result[2].split("/".toRegex()).dropLastWhile({ it.isEmpty() })[0].toInt() - 1)
-                indices.add(result[3].split("/".toRegex()).dropLastWhile({ it.isEmpty() })[0].toInt() - 1)
-            }
-
+        FACE_PATTERN3.toRegex().findAll(text).forEach {
+            val result = it.groups[0]!!.value.split(" ")
+            indices.add(result[1].split("/")[0].toInt() - 1)
+            indices.add(result[2].split("/")[0].toInt() - 1)
+            indices.add(result[3].split("/")[0].toInt() - 1)
         }
 
-        run {
-            val compile = Pattern.compile(FACE_PATTERN4)
-            val matcher = compile.matcher(text)
-
-            while (matcher.find()) {
-                val result = matcher.group().split(" ")
-                indices.add(result[1].split("//".toRegex()).dropLastWhile({ it.isEmpty() })[0].toInt() - 1)
-                indices.add(result[2].split("//".toRegex()).dropLastWhile({ it.isEmpty() })[0].toInt() - 1)
-                indices.add(result[3].split("//".toRegex()).dropLastWhile({ it.isEmpty() })[0].toInt() - 1)
-            }
-
+        FACE_PATTERN4.toRegex().findAll(text).forEach {
+            val result = it.groups[0]!!.value.split(" ")
+            indices.add(result[1].split("//")[0].toInt() - 1)
+            indices.add(result[2].split("//")[0].toInt() - 1)
+            indices.add(result[3].split("//")[0].toInt() - 1)
         }
 
         return BufferGeometry().apply {
