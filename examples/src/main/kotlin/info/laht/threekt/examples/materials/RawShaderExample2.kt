8564ab667beb7c266d3be5286f1ab406cb305cb2
Lars Ivar Hatledal <laht@hials.no>
2019-08-03 13:13:31 +0200

    Cleanup

--- examples/src/main/kotlin/info/laht/threekt/examples/materials/RawShaderExample2.kt
+++ examples/src/main/kotlin/info/laht/threekt/examples/materials/RawShaderExample2.kt
@@ -18,7 +18,6 @@ object RawShaderExample2 {
         uniform mat4 modelViewMatrix; // optional
         uniform mat4 projectionMatrix; // optional
         attribute vec3 position;
-        attribute vec4 color;
         void main()	{
             gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
         }
