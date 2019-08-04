80c7a8fc9cb65574a165a0a301e235cf60f17e12
Lars Ivar Hatledal <laht@hials.no>
2019-08-04 09:05:21 +0200

    Add new (non-working) shadow example

---
+++ examples/src/main/kotlin/info/laht/threekt/examples/lights/ShadowExample.kt
@@ -0,0 +1,70 @@
+package info.laht.threekt.examples.lights
+
+import info.laht.threekt.Canvas
+import info.laht.threekt.ShadowType
+import info.laht.threekt.cameras.PerspectiveCamera
+import info.laht.threekt.controls.OrbitControls
+import info.laht.threekt.geometries.PlaneBufferGeometry
+import info.laht.threekt.geometries.SphereBufferGeometry
+import info.laht.threekt.helpers.CameraHelper
+import info.laht.threekt.lights.PointLight
+import info.laht.threekt.materials.MeshPhongMaterial
+import info.laht.threekt.objects.Mesh
+import info.laht.threekt.renderers.GLRenderer
+import info.laht.threekt.scenes.Scene
+import kotlin.math.PI
+
+object ShadowExample {
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+
+        Canvas().use { canvas ->
+
+            val scene = Scene()
+
+            val camera = PerspectiveCamera()
+            camera.position.z = 20f
+
+            val renderer = GLRenderer(canvas.width, canvas.height)
+            renderer.shadowMap.enabled = true
+            renderer.shadowMap.type = ShadowType.PCFSoft
+
+            val light = PointLight(0xffffff, 1, 100)
+            light.position.set(0f, 10f, 0f)
+            light.castShadow = true
+            scene.add(light)
+
+            val sphereGeometry = SphereBufferGeometry(1f)
+            val sphereMaterial = MeshPhongMaterial().apply { color.set(0xff0000) }
+            val sphere = Mesh(sphereGeometry, sphereMaterial)
+            sphere.position.y = 2f
+            sphere.castShadow = true
+            sphere.receiveShadow = true
+            scene.add(sphere)
+
+            val planeGeometry = PlaneBufferGeometry(20f, 20f, 32, 32)
+            val planeMaterial = MeshPhongMaterial().apply { color.set(0x00ff00) }
+            val plane = Mesh(planeGeometry, planeMaterial)
+            plane.rotateX(-PI.toFloat() / 2)
+            plane.receiveShadow = true
+            scene.add(plane)
+
+            val helper = CameraHelper(light.shadow.camera)
+            scene.add(helper)
+
+            OrbitControls(camera, canvas)
+            fun render() {
+
+                renderer.render(scene, camera)
+                canvas.requestAnimationFrame { render() }
+
+            }
+
+            render()
+
+        }
+
+    }
+
+}
