984875e062934ca1a8aaed48a4dadb56d5ee9154
Lars Ivar Hatledal <laht@ntnu.no>
2019-08-09 14:41:27 +0200

    Update

--- core/src/jvmMain/kotlin/info/laht/threekt/loaders/GLTFLoader.kt
+++ core/src/jvmMain/kotlin/info/laht/threekt/loaders/GLTFLoader.kt
@@ -1,12 +1,14 @@
 package info.laht.threekt.loaders
 
 import com.google.gson.Gson
+import info.laht.threekt.LoaderUtils
 import info.laht.threekt.cameras.Camera
 import info.laht.threekt.cameras.OrthographicCamera
 import info.laht.threekt.cameras.PerspectiveCamera
 import info.laht.threekt.math.Matrix4
 import info.laht.threekt.math.Quaternion
 import info.laht.threekt.math.Vector3
+import info.laht.threekt.textures.Image
 import java.io.File
 
 class GLTFLoader {
@@ -14,14 +16,6 @@ class GLTFLoader {
     private var path: String? = null
     private var resourcePath: String? = null
 
-
-    fun load(path: String) {
-
-        val file = File(path)
-
-        parse(file.readText())
-
-    }
 
     fun setPath(path: String): GLTFLoader {
         this.path = path
@@ -33,22 +27,29 @@ class GLTFLoader {
         return this
     }
 
-    fun parse(data: String) {
+    fun load(url: String) {
+        val path = this.path ?: LoaderUtils.extractUrlBase(url)
+        parse(File(url).readText(), path)
+    }
 
-        println(Gson().fromJson(data, GLTF::class.java).cameras[0].getCamera())
-
+    fun parse(data: String, path: String) {
+        println(Gson().fromJson(data, GLTFData::class.java).images.getOrNull(0)?.getImage(path))
     }
 
 }
 
-private class GLTF {
+private class GLTFData {
 
     lateinit var asset: GLFTAsset
-    val scene: Int? = null
 
     val scenes: List<GLFTScene> = emptyList()
     val nodes: List<GLFTNode> = emptyList()
     val cameras: List<GLFTCamera> = emptyList()
+    val meshes: List<GLFTMesh> = emptyList()
+    val accessors: List<GLFTAccessor> = emptyList()
+    val textures: List<GLFTTexture> = emptyList()
+    val images: List<GLFTImage> = emptyList()
+    val samplers: List<GLFTSampler> = emptyList()
 
     data class GLFTAsset(
             val version: String,
@@ -56,12 +57,10 @@ private class GLTF {
             val copyright: String? = null
     )
 
-    class GLFTScene {
-
-        var name: String? = null
-        lateinit var nodes: IntArray
-
-    }
+    data class GLFTScene(
+            var name: String? = null,
+            val nodes: IntArray
+    )
 
     class GLFTNode {
 
@@ -122,6 +121,65 @@ private class GLTF {
                 orthographic != null -> OrthographicCamera()
                 else -> throw IllegalStateException("Neither perspective or orthographic..")
             }
+
+        }
+
+    }
+
+    data class GLFTMesh(
+            val primitives: List<GLFTPrimitive>
+    )
+
+    data class GLFTPrimitive(
+            val name: String? = null,
+            val indices: Int? = null,
+            val material: Int,
+            val mode: Int,
+            val attributes: Map<String, Int>
+    )
+
+    data class GLFTAccessor(
+            val bufferView: Int,
+            val byteOffset: Int,
+            val componentType: Int,
+            val count: Int,
+            val max: List<Float>? = null,
+            val min: List<Float>? = null,
+            val type: String
+
+    )
+
+    data class GLFTTexture(
+            val sampler: Int,
+            val source: Int
+    )
+
+    data class GLFTSampler(
+            val magFilter: Int? = null,
+            val minFilter: Int? = null,
+            val wrapS: Int? = null,
+            val wrapT: Int? = null
+    )
+
+    class GLFTImage {
+
+        private val uri: String? = null
+
+        private val bufferView: Int? = null
+        private val mimeType: Int? = null
+
+        fun getImage(baseDir: String): Image {
+
+            if (mimeType != null && bufferView != null) {
+                TODO()
+            }
+
+            if (uri == null) {
+                throw IllegalStateException("uri must not be null!")
+            }
+
+            val imageLocation = "$baseDir/$uri"
+            return ImageLoader.load(File(imageLocation))
 
         }
 
--- examples/src/main/kotlin/info/laht/threekt/examples/loaders/GLTFExample.kt
+++ examples/src/main/kotlin/info/laht/threekt/examples/loaders/GLTFExample.kt
@@ -7,7 +7,7 @@ object GLTFExample {
     @JvmStatic
     fun main(args: Array<String>) {
 
-        GLTFLoader().load(GLTFExample::class.java.classLoader.getResource("models/gltf/Duck/GLTF/Duck.gltf").file)
+        GLTFLoader().load(GLTFExample::class.java.classLoader.getResource("models/gltf/Duck/glTF/Duck.gltf").file)
 
     }
 
