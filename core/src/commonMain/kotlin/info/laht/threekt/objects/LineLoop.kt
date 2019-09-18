6d6b0f58f35cf6681d42c4089b1b83ce87effa67
Lars Ivar Hatledal <laht@ntnu.no>
2019-09-18 14:38:53 +0200

    Jvm interop

--- core/src/commonMain/kotlin/info/laht/threekt/objects/LineLoop.kt
+++ core/src/commonMain/kotlin/info/laht/threekt/objects/LineLoop.kt
@@ -2,8 +2,9 @@ package info.laht.threekt.objects
 
 import info.laht.threekt.core.BufferGeometry
 import info.laht.threekt.materials.LineBasicMaterial
+import kotlin.jvm.JvmOverloads
 
-class LineLoop(
-    geometry: BufferGeometry,
-    material: LineBasicMaterial
+class LineLoop @JvmOverloads constructor(
+        geometry: BufferGeometry? = null,
+        material: LineBasicMaterial? = null
 ) : Line(geometry, material)
