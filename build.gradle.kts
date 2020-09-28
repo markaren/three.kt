d69e00ac9af3c9d817d095dd8c78874b135f5806
Lars Ivar Hatledal <laht@hials.no>
2020-09-28 14:42:01 +0200

    Update kotlin and bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     `java-library`
-    kotlin("jvm") version "1.4.0" apply false
+    kotlin("jvm") version "1.4.10" apply false
     id("com.jfrog.bintray") version "1.8.4" apply false
 }
 
@@ -16,7 +16,7 @@ tasks.named<Wrapper>("wrapper") {
 println("Gradle version is ${gradle.gradleVersion}")
 
 group = "info.laht.threekt"
-version = "r1-ALPHA-21"
+version = "r1-ALPHA-22"
 
 println("Building three.kt $version")
 
