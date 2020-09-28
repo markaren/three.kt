ac0f923da6d4c8741ff97dcd853dc7e453c1bb29
Lars Ivar Hatledal <laht@hials.no>
2020-09-28 14:58:49 +0200

    Update ext

--- core/src/jvmMain/kotlin/info/laht/threekt/loaders/TextureLoaderExt.kt
+++ core/src/jvmMain/kotlin/info/laht/threekt/loaders/TextureLoaderExt.kt
@@ -2,21 +2,18 @@ package info.laht.threekt.loaders
 
 import info.laht.threekt.textures.Texture
 import java.io.File
-import java.io.InputStream
 import java.net.URL
 
 fun TextureLoader.load(url: URL, flipY: Boolean = true): Texture {
-    return load(url.openStream(), flipY)
+    val split = url.toExternalForm().split(".")
+    val type = split[split.lastIndex]
+    val tmp = createTempFile(suffix = ".$type")
+    tmp.writeBytes(url.openStream().use { it.readBytes() })
+    return load(tmp.absolutePath, flipY).also {
+        tmp.delete()
+    }
 }
 
 fun TextureLoader.load(file: File, flipY: Boolean = true): Texture {
     return load(file.absolutePath, flipY)
-}
-
-fun TextureLoader.load(`in`: InputStream, flipY: Boolean = true): Texture {
-    val tmp = createTempFile()
-    tmp.writeBytes(`in`.use { it.readBytes() })
-    return load(tmp.absolutePath, flipY).also {
-        tmp.delete()
-    }
 }
